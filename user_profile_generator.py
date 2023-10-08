bff44273610d98a8e3e14802fea98d1d88c0f3c9
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2023-10-08 22:45:06 +0100

    task completed - day 8

---
+++ user_profile_generator.py
@@ -0,0 +1,20 @@
+#Develop a program that asks the user for their name, age, and favorite color, stores these values in variables, and then prints out a user profile summary.
+
+print("Welcome to the User Profile Generator!")
+
+name = input("Please enter your name:")
+age = str(input("Please enter your age:"))
+favourite_color = input("Please enter your favorite color:")
+
+if name == name:
+    print("---------------------------------------------------------------------------------")
+    print("---------------------------------------------------------------------------------")
+    print("User Profile Summary:")
+    print("Name:" + name)
+
+    if age == age:
+        print("Age: " + age)
+
+        if favourite_color == favourite_color:
+            print("Favourite Color:  " + favourite_color)
+
